ddc0a67d2edfa2b6e44c6744cc83f4aa5e197729
sKabYY <skabyy@gmail.com>
2010-06-10 20:11:47 +0800

    Add some functions to handle client request

--- util/common.py
+++ util/common.py
@@ -15,9 +15,13 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Ailurus; if not, write to the Free Software Foundation, Inc.,
+# along with SBPFS; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
 
 def warning(msg):
     import sys
     print >>sys.stdout, 'Warning: ', msg
+
+def perror(msg):
+    import sys
+    print >>sys.stderr, 'Error: ', msg
--- util/parse.py
+++ util/parse.py
@@ -15,18 +15,30 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Ailurus; if not, write to the Free Software Foundation, Inc.,
+# along with SBPFS; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
 
+def gen_head(fl, d):
+    lines = []
+    lines.append(fl)
+    for key in d.keys():
+        line = '%s: %s' % (key, d[key])
+        lines.append(line)
+    lines.append('\r\n')
+    return '\r\n'.join(lines)
+
 def parse_head(s):
-    d = {}
-    lines = s.split('\r\n')
-    spl = lines[0].split()
-    d['Protocol'] = spl[0]
-    if len(spl) >= 2:
-        d['Response'] = spl[1]
-    for line in lines[1:]:
-        spl = line.split(': ', 1)
-        if not d.has_key(spl[0]):
-            d[spl[0]] = spl[1]
-    return d
+    try:
+        d = {}
+        lines = s.split('\r\n')
+        spl = lines[0].split()
+        d['Protocol'] = spl[0]
+        if len(spl) >= 2:
+            d['Response'] = spl[1]
+        for line in lines[1:]:
+            spl = line.split(': ', 1)
+            if not d.has_key(spl[0]):
+                d[spl[0]] = spl[1]
+        return d
+    except:
+        return None
